4119aafcc5330fb48406a93ee071ceeda652eb52
[SW5] 김성진 <kimsungjin927@gmail.com>
2023-08-20 01:22:12 +0900

    feat: login 수정

--- src/router/authRouter.js
+++ src/router/authRouter.js
@@ -25,8 +25,8 @@ router.post(
   "/login",
   asyncHandler(async (req, res, next) => {
     const { id, password } = req.body;
-    const userToken = await userService.getUserToken({ id, password });
-    res.cookie("loginToken", userToken).json(buildResponse({ msg: "로그인 성공" }));
+    const loginResult = await userService.getUserToken({ id, password });
+    res.cookie("loginToken", loginResult.userToken).json(buildResponse({ isLogin: true, isAdmin: loginResult.isAdmin }));
   })
 );
 
