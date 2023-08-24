dff1e1e92c36f593e1b1bea78f9fee224678c2ea
[SW5] 김성진 <kimsungjin927@gmail.com>
2023-08-24 23:03:06 +0900

    feat: 그룹 정보 가져오기 추가

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,14 @@ const express = require("express");
 const authRouter = require("./authRouter");
 const groupRouter = require("./groupRouter");
 const postRouter = require("./postRouter");
+const imageRouter = require("./imageRouter");
 // 버전1 라우터
 const v1Router = express.Router();
 
 v1Router.use("/auth", authRouter);
 v1Router.use("/group", groupRouter);
 v1Router.use("/post", postRouter);
+v1Router.use("/image", imageRouter);
 
 module.exports = {
   v1: v1Router, // API 버저닝
