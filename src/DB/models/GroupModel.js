611f19840099a558a9c447e03f7d975acfa9116a
[SW5] 김성진 <kimsungjin927@gmail.com>
2023-08-25 23:07:38 +0900

    feat:그룹 수정 추가

--- src/DB/models/GroupModel.js
+++ src/DB/models/GroupModel.js
@@ -1,7 +1,8 @@
 const { model } = require("mongoose");
-const { GroupSchema } = require("../schemas");
+const { GroupSchema, tagsSchema } = require("../schemas");
 
 const Group = model("groups", GroupSchema);
+const Tag = model("tags", tagsSchema);
 
 class GroupModel {
   async findByName(name) {
@@ -9,29 +10,62 @@ class GroupModel {
   }
 
   async create({ user_id, name, profile, maxMember, tag, duration }) {
-    return await Group.create({ name, profile, maxMember, tag, duration, leader: user_id });
+    const group = await Group.create({ name, profile, maxMember, duration, leader: user_id });
+    const group_id = group.group_id;
+    const tags = await Promise.all(
+      tag.map(async (item) => {
+        return await Tag.create({ group_id, tag: item });
+      })
+    );
+    return { group, tags };
   }
 
   async findById(group_id) {
     return await Group.findOne({ group_id: group_id });
   }
 
+  async getTags(group_id) {
+    const tags = await Tag.find({ group_id });
+    return tags.map((item) => item.tag);
+  }
+
   async getOldestGroups() {
     const groups = await Group.find();
-    return groups.map((item) => {
-      return { name: item.name, isRecruit: item.isRecruit, profile: item.profile, maxMember: item.maxMember, leadar: item.leader, createdAt: item.createdAt, group_id: item.group_id };
-    });
+    return await Promise.all(
+      groups.map(async (item) => {
+        let tags = await Tag.find({ group_id: item.group_id });
+        tags = tags.map((item) => item.tag);
+        return { name: item.name, isRecruit: item.isRecruit, profile: item.profile, maxMember: item.maxMember, leadar: item.leader, createdAt: item.createdAt, group_id: item.group_id, tags };
+      })
+    );
   }
 
   async getLatestGroups() {
     const groups = await Group.find().sort({ createdAt: -1 });
-    return groups.map((item) => {
-      return { name: item.name, isRecruit: item.isRecruit, profile: item.profile, maxMember: item.maxMember, leadar: item.leader, createdAt: item.createdAt, group_id: item.group_id };
-    });
+    return await Promise.all(
+      groups.map(async (item) => {
+        let tags = await Tag.find({ group_id: item.group_id });
+        tags = tags.map((item) => item.tag);
+        return { name: item.name, isRecruit: item.isRecruit, profile: item.profile, maxMember: item.maxMember, leadar: item.leader, createdAt: item.createdAt, group_id: item.group_id, tags };
+      })
+    );
   }
 
   async delete(group_id) {
     return await Group.deleteOne({ group_id: group_id });
+  }
+
+  async update({ group_id, name, profile, maxMember }) {
+    return await Group.findOneAndUpdate({ group_id }, { $set: { name, profile, maxMember } }, { new: true });
+  }
+
+  async updateTags({ group_id, tags }) {
+    return await Promise.all(
+      tags.map(async (item) => {
+        const tag = await Tag.findOneAndUpdate({ group_id }, { $set: { tag: item } }, { new: true });
+        return tag.tag;
+      })
+    );
   }
 }
 
