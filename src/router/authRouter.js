e8e1001b2aa19ea3649e073b4447ea2be0b1bdc9
[SW5] 김성진 <kimsungjin927@gmail.com>
2023-08-31 09:59:47 +0000

    Update authRouter.js

--- src/router/authRouter.js
+++ src/router/authRouter.js
@@ -24,7 +24,7 @@ router.post(
   asyncHandler(async (req, res, next) => {
     const { email, password } = req.body;
     const loginResult = await userService.getUserToken({ email, password });
-    res.cookie("loginToken", loginResult.token, { sameSite: "none", secure:true, httpOnly:true }).json(buildResponse({ isLogin: true, isAdmin: loginResult.isAdmin }));
+    res.cookie("loginToken", loginResult.token, { sameSite: "none", httpOnly:true }).json(buildResponse({ isLogin: true, isAdmin: loginResult.isAdmin }));
   })
 );
 
